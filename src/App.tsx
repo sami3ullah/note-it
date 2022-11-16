9779cab2a74d1c8f8fd013c808ca25a51005e31e
Muhammad Samiullah <msami398@gmail.com>
2022-11-17 00:41:36 +0500

    routes setup + 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { Container } from "@mantine/core"
+import { Routes, Route } from "react-router-dom"
+import Page404 from "./components/Page404/Page404"
 
 function App() {
 
   return (
-    <div className="App">
-      hi
-    </div>
+    <Container className="my-4" fluid>
+      <Routes>
+        <Route path="/" element={<h1>Hi</h1>} />
+        <Route path="/new" element={<h1>New</h1>} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Page404/>} />
+      </Routes>
+    </Container>
   )
 }
 
