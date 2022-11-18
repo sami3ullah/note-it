228faeac2ee39b42ed59e40b836379e2f84e1a51
Muhammad Samiullah <msami398@gmail.com>
2022-11-18 23:09:54 +0500

    removed sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,25 @@ import { Container } from "@mantine/core"
 import { Routes, Route } from "react-router-dom"
 import Page404 from "./components/Page404/Page404"
 import NewNote from "./components/NewNote/NewNote"
+import Header from "./components/Header/Header"
 
 function App() {
 
   return (
-    <Container className="my-4" size="lg">
-      <Routes>
-        <Route path="/" element={<h1>Hi</h1>} />
-        <Route path="/new" element={<NewNote />} />
-        <Route path="/:id">
-          <Route index element={<h1>Show</h1>} />
-          <Route path="edit" element={<h1>Edit</h1>} />
-        </Route>
-        <Route path="*" element={<Page404/>} />
-      </Routes>
-    </Container>
+    <div className="w-screen h-screen bg-lime-50">
+      <Header />
+      <Container size="lg">
+        <Routes>
+          <Route path="/" element={<h1>Hi</h1>} />
+          <Route path="/new" element={<NewNote />} />
+          <Route path="/:id">
+            <Route index element={<h1>Show</h1>} />
+            <Route path="edit" element={<h1>Edit</h1>} />
+          </Route>
+          <Route path="*" element={<Page404/>} />
+        </Routes>
+      </Container>
+    </div>
   )
 }
 
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+
+function Header() {
+  return (
+    <>
+      Navbar
+    </>
+  )
+}
+
+export default Header
