a7e06f1aef68fce00a7ef1b37aaffd0e19df8a88
Muhammad Samiullah <msami398@gmail.com>
2022-11-20 03:07:40 +0500

    Added animation and nav link

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,30 @@ import { Routes, Route } from "react-router-dom"
 import Page404 from "./components/Page404/Page404"
 import NewNote from "./components/NewNote/NewNote"
 import Header from "./components/Header/Header"
+import Sidebar from "./components/Sidebar/Sidebar"
+import Homepage from "./components/Homepage/Homepage"
+import { useState } from "react"
 
 function App() {
+  const [openSidebar, setOpenSidebar] = useState(false);
 
   return (
     <div className="w-screen h-screen bg-background">
-      <Header />
-      <Container size="lg">
-        <Routes>
-          <Route path="/" element={<h1>Hi</h1>} />
-          <Route path="/new" element={<NewNote />} />
-          <Route path="/:id">
-            <Route index element={<h1>Show</h1>} />
-            <Route path="edit" element={<h1>Edit</h1>} />
-          </Route>
-          <Route path="*" element={<Page404/>} />
-        </Routes>
-      </Container>
+      <Header setOpenSidebar={setOpenSidebar} openSidebar={openSidebar}/> 
+      <div className="flex">
+        <Sidebar openSidebar={openSidebar}/>
+        <div className="container mx-auto border-tertiary border">
+          <Routes>
+            <Route path="/" element={<Homepage/>} />
+            <Route path="/new" element={<NewNote />} />
+            <Route path="/:id">
+              <Route index element={<h1>Show</h1>} />
+              <Route path="edit" element={<h1>Edit</h1>} />
+            </Route>
+            <Route path="*" element={<Page404/>} />
+          </Routes>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,18 @@
 
-function Header() {
+function Header({setOpenSidebar, openSidebar}:any) {
+
+  const toggle = () => {
+    setOpenSidebar(!openSidebar)
+  }
+
   return (
     <nav className="flex items-center justify-between px-4 sm:px-7 py-2 h-15 bg-foreground shadow-sm">
       <div className="flex items-center gap-3">
         {/* menu */}
-        <button className="rounded-full w-10 h-10 hover:bg-hover flex items-center justify-center transition-all">
+        <button onClick={toggle} className="rounded-full w-10 h-10 hover:bg-hover flex items-center justify-center transition-all">
           <div className="space-y-2">
-            <span className="block w-7 h-1 bg-secondary bg-opacity-60"></span>
-            <span className="block w-7 h-1 bg-secondary bg-opacity-60"></span>
+            <span className="block w-5 h-0.5 bg-secondary bg-opacity-60"></span>
+            <span className="block w-7 h-0.5 bg-secondary bg-opacity-60"></span>
           </div>
         </button>
         <div className="font-bold text-2xl">Note it</div>
